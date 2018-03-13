096a28938acb104607da5166083a07cec732c037
Victor Requena <svictoreq@gmail.com>
2018-03-13 13:18:03 -0400

    Add require check functionality

---
+++ js/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	const portInputs = [...$('#portfolio input')];
+	const accountInputs = [...$('#account input')];
+	const inputs = [...$('input')];
+
+	const disableAccInputs = function disableAccInputs(value) {
+		for (let i = accountInputs.length - 1; i >= 0; i--) {
+			if (typeof value !== "boolean") {
+				throw 'Invalid type of parameter, must be a boolean';
+			}
+			$(accountInputs[i]).prop('disabled', value);
+		}
+	}
+
+	// Required checking
+	inputs.forEach(input => {
+		$(input).on('focusout', function() {
+			if (!this.value) {
+				$(this).addClass('required');
+				$(this).attr('placeholder', 'REQUIRED');
+			} else {
+				$(this).removeClass('required');
+				$(this).attr('placeholder', '');
+			}
+		});
+
+		$(input).on('keypress', function() {
+			$(this).removeClass('required');
+			$(this).attr('placeholder', '');
+		});
+	});
+
+	// Check if portfolio inputs are filled
+	disableAccInputs(true);
+	portInputs.forEach(input => {
+		$(input).on('keyup', function() {
+			if (portInputs.every(input => input.value)) {
+				disableAccInputs(false);
+			} else {
+				disableAccInputs(true);
+			}
+		});
+	})
+});
